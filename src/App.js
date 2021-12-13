fe78eb1b357d4274cec2659ae5ee2671c56cfef8
heraldofortuna <heraldofortuna@gmail.com>
2021-12-13 00:51:30 -0500

    El componente del card fue creado y sus estilos fueron implementados

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import Header from "./components/Header";
 import Tab from "./components/Tab";
+import Card from "./components/Card";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Tab />
+      <Card />
     </div>
   );
 };
---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import "../styles/Card.css";
+import clock from "../assets/clock.svg";
+import heart from "../assets/heart.svg";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="card__content">
+        <div className="card__content__time">
+          <img
+            className="card__content__time__icon"
+            src={clock}
+            alt="Publication time by author icon"
+          />
+          <p className="card__content__time__text">2 hours ago by author</p>
+        </div>
+        <p className="card__content__title">
+          Event-driven state management in React using Storeon
+        </p>
+      </div>
+      <div className="card__fave">
+        <img className="card__fave__icon" src={heart} alt="Favorite publication icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
